02d2ddcda78a2b32cba3b625ee5b1b6eb887a5ce
readj4vvat <liamlee7797@gmail.com>
2024-04-23 16:21:32 +0700

    add input logic

--- newsletter-sign-up-with-success-message-main/js/main.js
+++ newsletter-sign-up-with-success-message-main/js/main.js
@@ -1,5 +1,10 @@
 const emailInput = document.getElementsByClassName("card__body__email__input")[0];
 const emailError = document.getElementsByClassName("card__body__email__error")[0];
+const textEmail = document.getElementsByClassName("card__body__text--email")[0];
+const cardSignUp = document.getElementById("card-signup");
+const cardSuccess = document.getElementById("card-success");
+const submitBtn = document.getElementById("submitBtn");
+const dismissBtn = document.getElementById("dismissBtn");
 const showError = () => {
   if (!(emailInput.checkValidity())) {
     emailError.style.display = "block";
@@ -9,3 +14,16 @@ const showError = () => {
 }
 
 emailInput.addEventListener("input", showError);
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (emailInput.checkValidity() && emailInput.value !== "") {
+    cardSignUp.classList.toggle("hide");
+    cardSuccess.classList.toggle("hide");
+    textEmail.innerHTML = emailInput.value;
+  }
+});
+dismissBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  cardSignUp.classList.toggle("hide");
+  cardSuccess.classList.toggle("hide");
+});
